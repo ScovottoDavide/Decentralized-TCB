37febac483ecac229cdf20c313ed8082e4670dbf
ScovottoDavide <scovottodavide@gmail.com>
2022-05-01 18:23:39 +0200

    Quote. Completed (TODO: fix bug in print_pcrs)

--- TPA/tpm2_quote.c
+++ TPA/tpm2_quote.c
@@ -17,7 +17,7 @@ struct tpm_quote_ctx {
     TPM2B_DATA qualification_data;
     TPML_PCR_SELECTION pcr_selections;
     TPMS_CAPABILITY_DATA cap_data;
-    //tpm2_pcrs pcrs;
+    tpm2_pcrs pcrs;
     //tpm2_convert_pcrs_output_fmt pcrs_format;
     TPMT_SIG_SCHEME in_scheme;
     TPMI_ALG_SIG_SCHEME sig_scheme;
@@ -44,7 +44,7 @@ struct tpm_quote_ctx {
 
 static tpm_quote_ctx ctx = {
     .sig_hash_algorithm = TPM2_ALG_NULL,
-    //.qualification_data = TPM2B_EMPTY_INIT,
+    .qualification_data = TPM2B_EMPTY_INIT,
     //.pcrs_format = pcrs_output_format_serialized,
     .in_scheme.scheme = TPM2_ALG_NULL,
     .sig_scheme = TPM2_ALG_NULL,
@@ -117,7 +117,11 @@ bool pcr_parse_list(const char *str, size_t len, TPMS_PCR_SELECTION *pcr_select)
 
         pcr_select->pcrSelect[pcr / 8] |= (1 << (pcr%8));
     }while(str);
-
+    /*int i;
+    // print PCR bits
+    for(i = 0; i<3; i++)
+      fprintf(stdout, "%02x ", pcr_select->pcrSelect[i]);
+    fprintf(stdout, "\n");*/
     return true;
 }
 
@@ -300,6 +304,401 @@ TSS2_RC tpm2_get_signature_scheme(ESYS_CONTEXT *ectx, ESYS_TR key_handle, TPMI_A
   return TSS2_RC_SUCCESS;
 }
 
+static void shrink_pcr_selection(TPML_PCR_SELECTION *s) {
+    UINT32 i, j;
+
+    //seek for the first empty item
+    for (i = 0; i < s->count; i++)
+        if (!s->pcrSelections[i].hash)
+            break;
+    j = i + 1;
+
+    for (; i < s->count; i++) {
+        if (!s->pcrSelections[i].hash) {
+            for (; j < s->count; j++)
+                if (s->pcrSelections[j].hash)
+                    break;
+            if (j >= s->count)
+                break;
+
+            memcpy(&s->pcrSelections[i], &s->pcrSelections[j],
+                    sizeof(s->pcrSelections[i]));
+            s->pcrSelections[j].hash = 0;
+            j++;
+        }
+    }
+
+    s->count = i;
+}
+
+bool pcr_check_pcr_selection(TPMS_CAPABILITY_DATA *cap_data, TPML_PCR_SELECTION *pcr_sel){
+  int i, j, k;
+
+  for (i = 0; i < pcr_sel->count; i++) {
+        for (j = 0; j < cap_data->data.assignedPCR.count; j++) {
+            if (pcr_sel->pcrSelections[i].hash
+                    == cap_data->data.assignedPCR.pcrSelections[j].hash) {
+                for (k = 0; k < pcr_sel->pcrSelections[i].sizeofSelect; k++)
+                    pcr_sel->pcrSelections[i].pcrSelect[k] &=
+                            cap_data->data.assignedPCR.pcrSelections[j].pcrSelect[k];
+                break;
+            }
+        }
+
+      if(j >= cap_data->data.assignedPCR.count){
+        // ignore unsupported bank/algorithm
+        pcr_sel->pcrSelections[i].hash = 0;
+      }
+  }
+
+  shrink_pcr_selection(pcr_sel);
+  if(pcr_sel->count == 0) // should be 2 (sha1 + sha256 banks)
+    return false;
+
+  return true;
+}
+
+// Check if the pcr bit is set --> return false if it is set (1) else return true if (0)
+bool pcr_unset_pcr_sections(TPML_PCR_SELECTION *s) {
+  UINT32 i, j;
+
+  for(i=0; i<s->count; i++){
+    for(j=0; j<s->pcrSelections[i].sizeofSelect; i++)
+      if(s->pcrSelections[i].pcrSelect[j]) // the bit is set
+        return false;
+  }
+
+  return true;
+}
+
+void pcr_update_pcr_selections(TPML_PCR_SELECTION *s1, TPML_PCR_SELECTION *s2){
+  UINT32 i1, i2, j;
+    for (i2 = 0; i2 < s2->count; i2++) {
+        for (i1 = 0; i1 < s1->count; i1++) {
+            if (s2->pcrSelections[i2].hash != s1->pcrSelections[i1].hash)
+                continue;
+
+            for (j = 0; j < s1->pcrSelections[i1].sizeofSelect; j++)
+                s1->pcrSelections[i1].pcrSelect[j] &= ~s2->pcrSelections[i2].pcrSelect[j];
+        }
+    }
+}
+
+TSS2_RC pcr_read_pcr_values(ESYS_CONTEXT *esys_context, TPML_PCR_SELECTION *pcr_select, tpm2_pcrs *pcrs){
+
+  TPML_PCR_SELECTION pcr_selection_tmp_in;
+  TPML_PCR_SELECTION *pcr_selection_out;
+  int pcr_update_counter;
+
+  // Copy in local pcr_selection_tmp
+  memcpy(&pcr_selection_tmp_in, pcr_select, sizeof(pcr_selection_tmp_in));
+
+  // read pcrs
+  pcrs->count = 0;
+  do{
+    TPML_DIGEST *value;
+    TSS2_RC res = Esys_PCR_Read(esys_context, ESYS_TR_NONE, ESYS_TR_NONE, ESYS_TR_NONE,
+                    &pcr_selection_tmp_in, &pcr_update_counter, &pcr_selection_out, &value);
+    if(res != TSS2_RC_SUCCESS){
+      fprintf(stderr, "Error while reading PCRs \n");
+      return TSS2_ESYS_RC_BAD_VALUE;
+    }
+
+    pcrs->pcr_values[pcrs->count] = *value;
+
+    free(value);
+
+    //unmask pcrSelectionOut bits from pcrSelectionIn
+    pcr_update_pcr_selections(&pcr_selection_tmp_in, pcr_selection_out);
+
+    free(pcr_selection_out);
+
+  }while(++pcrs->count < sizeof(pcrs->pcr_values) && !pcr_unset_pcr_sections(&pcr_selection_tmp_in));
+
+  if (pcrs->count >= sizeof(pcrs->pcr_values) && !pcr_unset_pcr_sections(&pcr_selection_tmp_in)) {
+       fprintf(stderr, "Too much pcrs to get!\n");
+       return TSS2_ESYS_RC_BAD_VALUE;
+   }
+
+   return TSS2_RC_SUCCESS;
+}
+
+bool pcr_print(TPML_PCR_SELECTION *pcr_select, tpm2_pcrs *pcrs){
+  UINT32 i;
+  size_t vi = 0;  /* value index */
+  UINT32 di = 0;  /* digest index */
+
+  fprintf(stdout, "pcrs: \n");
+  // Go through all PCRs in each bank
+  for(i = 0; i < pcr_select->count; i++){
+    const char *alg_name;
+    if(pcr_select->pcrSelections[i].hash == TPM2_ALG_SHA1){
+      alg_name = malloc(strlen("sha1")*sizeof(char));
+      alg_name = "sha1";
+    } else if(pcr_select->pcrSelections[i].hash == TPM2_ALG_SHA256) {
+      alg_name = malloc(strlen("sha256")*sizeof(char));
+      alg_name = "sha256";
+    }
+
+    fprintf(stdout, "  %s\n", alg_name);
+    // Go through all PCRs in this banks
+    unsigned int pcr_id;
+    for(pcr_id = 0; pcr_id < pcr_select->pcrSelections[i].sizeofSelect * 8u; pcr_id++){
+      // skip unset pcrs (bit = 0)
+      if(!(pcr_select->pcrSelections[i].pcrSelect[((pcr_id) / 8)] & (1 << ((pcr_id) % 8)))){
+        continue;
+      }
+
+      if(vi >= pcrs->count || di >= pcrs->pcr_values[vi].count){
+        fprintf(stderr, "Trying to print but nothing more! di: %d, count: %d\n", di, pcrs->pcr_values[vi].count);
+        return false;
+      }
+
+      // Print PCR ID
+      fprintf(stdout, "   %-2d: 0x", pcr_id);
+
+      // Print current PRC content (digest value)
+      TPM2B_DIGEST *d = &pcrs->pcr_values[vi].digests[di];
+      int k;
+      for(k = 0; k < d->size; k++){
+        fprintf(stdout, "%02X", d->buffer[k]);
+      }
+      fprintf(stdout, "\n");
+
+      if(++di >= pcrs->pcr_values[vi].count){
+        di = 0;
+        ++vi;
+      }
+    }
+  }
+
+  return true;
+}
+
+bool tpm2_openssl_hash_pcr_banks(TPMI_ALG_HASH hash_alg, TPML_PCR_SELECTION *pcr_select, tpm2_pcrs *pcrs, TPM2B_DIGEST *digest){
+  UINT32 vi = 0, di = 0, i;
+
+  if(hash_alg != TPM2_ALG_SHA256){
+    fprintf(stderr, "Wrong HashAlgo\n");
+    return false;
+  }
+
+  const EVP_MD *md = EVP_sha256(); // ctx.sig_hash_algorithm = TPM2_ALG_SHA256
+  if(!md)
+    return false;
+
+  EVP_MD_CTX *mdctx = EVP_MD_CTX_create();
+  if(!mdctx)
+    return false;
+
+  int res = EVP_DigestInit_ex(mdctx, md, NULL);
+  if(!res){
+    EVP_MD_CTX_destroy(mdctx);
+    return false;
+  }
+
+  // Loop through all PCR/hash banks
+  for(i = 0; i < pcr_select->count; i++){
+
+    // Loop through all PCRs in this bank
+    unsigned int pcr_id;
+    for(pcr_id = 0; pcr_id < pcr_select->pcrSelections[i].sizeofSelect * 8u; pcr_id++){
+      // skip unset pcrs (bit = 0)
+      if(!(pcr_select->pcrSelections[i].pcrSelect[((pcr_id) / 8)] & (1 << ((pcr_id) % 8)))){
+        continue;
+      }
+
+      if(vi >= pcrs->count || di >= pcrs->pcr_values[vi].count){
+        fprintf(stderr, "Trying to print but nothing more! di: %d, count: %d\n", di, pcrs->pcr_values[vi].count);
+        return false;
+      }
+
+      // Update running digest (to compare with quote)
+      TPM2B_DIGEST *b = &pcrs->pcr_values[vi].digests[di];
+      res = EVP_DigestUpdate(mdctx, b->buffer, b->size);
+      if(!res){
+        EVP_MD_CTX_destroy(mdctx);
+        return false;
+      }
+
+      if (++di < pcrs->pcr_values[vi].count)
+                continue;
+      di = 0;
+      if (++vi < pcrs->count)
+                continue;
+    }
+  }
+
+  // Finalize running digest
+  unsigned size = EVP_MD_size(md);
+  res = EVP_DigestFinal_ex(mdctx, digest->buffer, &size);
+  if(!res){
+    EVP_MD_CTX_destroy(mdctx);
+    return false;
+  }
+
+  digest->size = size;
+  EVP_MD_CTX_destroy(mdctx);
+  return true;
+}
+
+bool tpm2_util_verify_digests(TPM2B_DIGEST *quoteDigest, TPM2B_DIGEST *pcr_digest) {
+  // Sanity check -- they should at least be same size!
+    if (quoteDigest->size != pcr_digest->size) {
+        fprintf(stderr, "FATAL ERROR: PCR values failed to match quote's digest!\n");
+        return false;
+    }
+    // Compare running digest with quote's digest
+    int k;
+    for (k = 0; k < quoteDigest->size; k++) {
+        if (quoteDigest->buffer[k] != pcr_digest->buffer[k]) {
+            fprintf(stderr, "FATAL ERROR: PCR values failed to match quote's digest!\n");
+            return false;
+        }
+    }
+    return true;
+}
+
+TSS2_RC get_digest_from_quote(TPM2B_ATTEST *quoted, TPMS_ATTEST *attest){
+
+  size_t offset = 0;
+  TSS2_RC res = Tss2_MU_TPMS_ATTEST_Unmarshal(quoted->attestationData, quoted->size, &offset, attest);
+  if(res != TSS2_RC_SUCCESS){
+    fprintf(stderr, "Cannot get digest from quote\n");
+    return TSS2_ESYS_RC_BAD_VALUE;
+  }
+  return TSS2_RC_SUCCESS;
+}
+
+bool tpm2_convert_sig_save(TPMT_SIGNATURE *signature, const char *path) {
+  UINT8 *buffer;
+  UINT16 size;
+
+  // convert_sig for TPM2_ALG_RSASSA
+  size = signature->signature.rsassa.sig.size;
+  buffer = malloc(size);
+  if(!buffer){
+    fprintf(stderr, "OOM\n");
+    return false;
+  }
+  memcpy(buffer, signature->signature.rsassa.sig.buffer, size);
+
+  if(!path){
+    fprintf(stderr, "No path specified\n");
+    return false;
+  }
+  FILE *fp = fopen(path, "wb+");
+  if(!fp){
+    fprintf(stderr, "Could not open file %s\n", path);
+    return false;
+  }
+  size_t wrote = 0, index = 0;
+  do{
+    wrote = fwrite(&buffer[index], 1, size, fp);
+    if(wrote != size)
+      return false;
+    size-=wrote;
+    index+=wrote;
+  }while(size > 0);
+
+  fclose(fp);
+  free(buffer);
+  return true;
+}
+
+bool tpm2_save_message_out(const char *path, UINT8 *buf, UINT16 size) {
+  if(!buf) return false;
+
+  if(!path){
+    fprintf(stderr, "No path specified\n");
+    return false;
+  }
+
+  FILE *fp = fopen(path, "wb+");
+  if(!fp){
+    fprintf(stderr, "Could not open file %s\n", path);
+    return false;
+  }
+  size_t wrote = 0, index = 0;
+  do{
+    wrote = fwrite(&buf[index], 1, size, fp);
+    if(wrote != size)
+      return false;
+    size-=wrote;
+    index+=wrote;
+  }while(size > 0);
+
+  fclose(fp);
+  return true;
+}
+
+bool pcr_fwrite_serialized(const TPML_PCR_SELECTION *pcr_select, const tpm2_pcrs *ppcrs, FILE *output_file) {
+
+    TPML_PCR_SELECTION pcr_selection = *pcr_select;
+    for (UINT32 i = 0; i < pcr_selection.count; i++) {
+        TPMS_PCR_SELECTION *p = &pcr_selection.pcrSelections[i];
+        p->hash = htole16(p->hash);
+    }
+    pcr_selection.count = htole32(pcr_selection.count);
+
+    // Export TPML_PCR_SELECTION structure to pcr outfile
+    size_t fwrite_len = fwrite(&pcr_selection, sizeof(TPML_PCR_SELECTION),
+        1, output_file);
+    if (fwrite_len != 1) {
+        fprintf(stderr, "write to output file failed\n");
+        return false;
+    }
+
+    // Export PCR digests to pcr outfile
+    UINT32 count = htole32(ppcrs->count);
+    fwrite_len = fwrite(&count, sizeof(UINT32), 1, output_file);
+    if (fwrite_len != 1) {
+        fprintf(stderr, "write to output file failed");
+        return false;
+    }
+
+    tpm2_pcrs pcrs = *ppcrs;
+    for (size_t j = 0; j < pcrs.count; j++) {
+        TPML_DIGEST *pcr_value = &pcrs.pcr_values[j];
+
+        for (size_t k = 0; k < pcr_value->count; k++) {
+            TPM2B_DIGEST *p = &pcr_value->digests[k];
+            p->size = htole16(p->size);
+        }
+        pcr_value->count = htole32(pcr_value->count);
+        fwrite_len = fwrite(pcr_value, sizeof(TPML_DIGEST), 1, output_file);
+        if (fwrite_len != 1) {
+            fprintf(stderr, "write to output file failed");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+static TSS2_RC write_output_files(void) {
+  bool is_success = true;
+  bool result = true;
+
+  if(ctx.signature_path){
+    // signature format plain
+    result = tpm2_convert_sig_save(ctx.signature, ctx.signature_path);
+    if(!result) is_success = result;
+  }
+
+  if(ctx.message_path) {
+    result = tpm2_save_message_out(ctx.message_path, (UINT8*) &ctx.quoted->attestationData, ctx.quoted->size);
+    if(!result) is_success = result;
+  }
+
+  if(ctx.pcr_output){
+    result = pcr_fwrite_serialized(&ctx.pcr_selections, &ctx.pcrs, ctx.pcr_output);
+    if(!result) is_success = result;
+  }
+
+  return is_success ? TSS2_RC_SUCCESS : TSS2_ESYS_RC_BAD_VALUE;
+}
+
 TSS2_RC tpm2_quote_internal(ESYS_CONTEXT *esys_context, tpm2_loaded_object *quote_obj, TPMT_SIG_SCHEME *in_scheme, TPM2B_DATA *qualifying_data,
   TPML_PCR_SELECTION *pcr_select, TPM2B_ATTEST **quoted,  TPMT_SIGNATURE **signature){
 
@@ -404,5 +803,52 @@ TSS2_RC tpm2_quote(ESYS_CONTEXT *esys_ctx) {
       fprintf(stdout, "%02x", ctx.signature->signature.rsassa.sig.buffer[i]);
     fprintf(stdout, "\n");
 
-    return TSS2_RC_SUCCESS;
+    if(ctx.pcr_output){
+      // Filter out invalid/unavailable PCR Selections
+      if(!pcr_check_pcr_selection(&ctx.cap_data, &ctx.pcr_selections)){
+        fprintf(stderr, "Cannot filter unavailable PCR values for quote!\n");
+        return TSS2_ESYS_RC_BAD_VALUE;
+      }
+
+      // Read PCRs from TPM
+      tss_r = pcr_read_pcr_values(esys_ctx, &ctx.pcr_selections, &ctx.pcrs);
+      if(tss_r != TSS2_RC_SUCCESS){
+        fprintf(stderr, "Error while reading PCRs from TPM\n");
+        return TSS2_ESYS_RC_BAD_VALUE;
+      }
+
+      tss_r = get_digest_from_quote(ctx.quoted, &ctx.attest);
+      if(tss_r != TSS2_RC_SUCCESS){
+        fprintf(stderr, "Error while trying to get digest from quote\n");
+        return TSS2_ESYS_RC_BAD_VALUE;
+      }
+
+      res = pcr_print(&ctx.pcr_selections, &ctx.pcrs); // BUG IN PRINTING THE PCRS!!
+      if(!res){
+        fprintf(stderr, "Error while printing PCRS to stdout\n");
+        return TSS2_ESYS_RC_BAD_VALUE;
+      }
+
+      // Calculate the digest from our selected PCR values (to ensure correctness)
+      TPM2B_DIGEST pcr_digest = TPM2B_TYPE_INIT(TPM2B_DIGEST, buffer);
+      res = tpm2_openssl_hash_pcr_banks(ctx.sig_hash_algorithm, &ctx.pcr_selections, &ctx.pcrs, &pcr_digest);
+      if(!res){
+        fprintf(stderr, "Error while computing hash PCR values needed for ensure correctness of quote\n");
+        return TSS2_ESYS_RC_BAD_VALUE;
+      }
+
+      fprintf(stdout, "calcDigest: ");
+      for(i = 0; i < pcr_digest.size; i++)
+        fprintf(stdout, "%02x", pcr_digest.buffer[i]);
+      fprintf(stdout, "\n");
+
+      // Make sure digest from quote matches calculated PCR digest
+      res = tpm2_util_verify_digests(&ctx.attest.attested.quote.pcrDigest, &pcr_digest);
+      if(!res){
+        fprintf(stderr, "Error: calculated PCRs digest does not match PCRs digest in the quote\n");
+        return TSS2_ESYS_RC_BAD_VALUE;
+      }
+    }
+
+    return write_output_files();
 }
--- TPA/tpm2_quote.h
+++ TPA/tpm2_quote.h
@@ -2,13 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <openssl/evp.h>
 #include <tss2/tss2_esys.h>
 #include <tss2/tss2_tpm2_types.h>
+#include <tss2/tss2_mu.h>
+
+#define BUFFER_SIZE(type, field) (sizeof((((type *)NULL)->field)))
+#define TPM2B_TYPE_INIT(type, field) { .size = BUFFER_SIZE(type, field), }
+
+#define TPM2B_INIT(xsize) { .size = xsize, }
+#define TPM2B_EMPTY_INIT TPM2B_INIT(0)
 
 typedef struct tpm2_algorithm tpm2_algorithm;
 struct tpm2_algorithm {
     int count;
     TPMI_ALG_HASH alg[TPM2_NUM_PCR_BANKS];
+};
+
+typedef struct tpm2_pcrs tpm2_pcrs;
+struct tpm2_pcrs {
+    size_t count;
+    TPML_DIGEST pcr_values[TPM2_MAX_PCRS];
 };
 
 bool pcr_parse_list(const char *str, size_t len, TPMS_PCR_SELECTION *pcr_select);
@@ -18,6 +32,23 @@ TSS2_RC pcr_get_banks(ESYS_CONTEXT *esys_context, TPMS_CAPABILITY_DATA *capabili
 TSS2_RC tpm2_public_to_scheme(ESYS_CONTEXT *ectx, ESYS_TR key, TPMI_ALG_PUBLIC *type, TPMT_SIG_SCHEME *sigscheme);
 //tpm2_alg_util_get_signature_scheme
 TSS2_RC tpm2_get_signature_scheme(ESYS_CONTEXT *ectx, ESYS_TR key_handle, TPMI_ALG_HASH *halg, TPMI_ALG_SIG_SCHEME sig_scheme, TPMT_SIG_SCHEME *scheme);
+
+static void shrink_pcr_selection(TPML_PCR_SELECTION *s);
+bool pcr_check_pcr_selection(TPMS_CAPABILITY_DATA *cap_data, TPML_PCR_SELECTION *pcr_sel);
+bool pcr_unset_pcr_sections(TPML_PCR_SELECTION *s);
+void pcr_update_pcr_selections(TPML_PCR_SELECTION *s1, TPML_PCR_SELECTION *s2);
+TSS2_RC pcr_read_pcr_values(ESYS_CONTEXT *esys_context, TPML_PCR_SELECTION *pcr_select, tpm2_pcrs *pcrs);
+bool pcr_print(TPML_PCR_SELECTION *pcr_select, tpm2_pcrs *pcrs);
+
+bool tpm2_openssl_hash_pcr_banks(TPMI_ALG_HASH hash_alg, TPML_PCR_SELECTION *pcr_select, tpm2_pcrs *pcrs, TPM2B_DIGEST *digest);
+bool tpm2_util_verify_digests(TPM2B_DIGEST *quoteDigest, TPM2B_DIGEST *pcr_digest);
+
+bool tpm2_convert_sig_save(TPMT_SIGNATURE *signature, const char *path);
+bool tpm2_save_message_out(const char *path, UINT8 *buf, UINT16 size);
+bool pcr_fwrite_serialized(const TPML_PCR_SELECTION *pcr_select, const tpm2_pcrs *ppcrs, FILE *output_file);
+static TSS2_RC write_output_files(void);
+
+TSS2_RC get_digest_from_quote(TPM2B_ATTEST *quoted, TPMS_ATTEST *attest);
 
 // tpm2_quote_internal in createak_util.h --> to fix dependencies!!!!!!!!!
 
