3b30eed8891d8a09af3366fbe1f52dfbceea40b7
ScovottoDavide <scovottodavide@gmail.com>
2022-11-25 14:51:04 +0100

    Added 2 more raspis in the .sh script

--- HeartBeat-Utils/generate_and_dispatch_indexes.sh
+++ HeartBeat-Utils/generate_and_dispatch_indexes.sh
@@ -30,6 +30,8 @@ mv heartbeat_write.json ../heartbeat_WAM/
     #echo "--> Sending TPA_index_node$i and RA_index_node$i file to node"
     scp TPA_index_node1.json RA_index_node1.json pi@192.168.0.115:/etc/tc
     scp TPA_index_node2.json RA_index_node2.json pi@192.168.0.114:/etc/tc
+    scp TPA_index_node3.json RA_index_node3.json pi@192.168.0.105:/etc/tc
+    scp TPA_index_node4.json RA_index_node4.json pi@192.168.0.108:/etc/tc
 #done
 rm TPA_index_node* RA_index_node*
 
